3b7183c5832211d31ac4f9db6446874ef4f0c2ed
sriram4dante <veeraghanta.sriram@gmail.com>
2015-06-10 11:37:00 +0530

    Working

---
+++ blog.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+import os
+from model import *
+
+DEBUG = True
+SECRET_KEY = 'hello_world'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def show_entries():
+    data = BlogPost.select()
+    return render_template('show_entries.html', data=data)
+
+@app.route('/add', methods=['GET','POST'])
+def add_entry():
+    if request.method == 'POST':
+    	entry = BlogPost(title=request.form['title'],text=request.form['description'])
+    	entry.save()
+    	flash('New entry was successfully posted')
+    	return redirect(url_for('show_entries'))
+    return render_template('add.html')
+
+
+if __name__ == '__main__':
+    app.run()
